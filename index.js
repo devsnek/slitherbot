fed1737053d2997a042d828b86f499506a42e17a
Gus Caplan <me@gus.host>
2017-09-25 10:04:01 -0500

    eslint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const request = require('snekfetch')
+const request = require('snekfetch');
 const cheerio = require('cheerio');
 const URL = require('url');
 const version = require('./package.json').version;
@@ -6,7 +6,7 @@ const EventEmitter = require('events');
 
 class Slitherbot extends EventEmitter {
   get _useragent() {
-    return `slitherbot/${version} (Node.js/${process.version}, node-fetcher/${fetcher.version})`;
+    return `slitherbot/${version} (Node.js/${process.version}, snekfetch/${request.version})`;
   }
 
   async search(start, search) {
@@ -20,8 +20,8 @@ class Slitherbot extends EventEmitter {
       count++;
       const body = await request.get(url)
         .set('User-Agent', this._useragent)
-        .then(r => r.text)
-        .catch(r => r);
+        .then((r) => r.text)
+        .catch((r) => r);
       if (typeof body !== 'string') continue;
       if (search && body.toLowerCase().includes(search.toLowerCase())) {
         return {
@@ -37,11 +37,11 @@ class Slitherbot extends EventEmitter {
       const $ = cheerio.load(body);
       // this is super inefficient but i'm too lazy to make something better
       const links = Array.from($('a'))
-        .filter(l => l.attribs && l.attribs.href)
-        .map(l => URL.parse(l.attribs.href))
-        .filter(u => u.host && ['https:', 'http:'].includes(u.protocol))
-        .map(u => URL.format(u))
-        .filter(l => !used.includes(l) && !toVisit.includes(l));
+        .filter((l) => l.attribs && l.attribs.href)
+        .map((l) => URL.parse(l.attribs.href))
+        .filter((u) => u.host && ['https:', 'http:'].includes(u.protocol))
+        .map((u) => URL.format(u))
+        .filter((l) => !used.includes(l) && !toVisit.includes(l));
       toVisit.push(...links);
       used.push(url);
       if (this.listenerCount('crawl')) {
